271e750d9051d3fe943c7a53bcc9be888ad18f98
Vitalii <votosi197@gmail.com>
2022-11-10 20:43:50 +0100

    added: stub function

--- MelnykovNotes/urls.py
+++ MelnykovNotes/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from registration.views import register_user
+from registration.views import login_user
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('registration/', register_user, name='registration'),
+    path('login/', login_user, name='login')
 ]
