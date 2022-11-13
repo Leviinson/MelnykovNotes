68c7edf8e46c675f0f22f92072b9628c2cacc004
Vitalii <votosi197@gmail.com>
2022-11-13 14:30:01 +0100

    updated: app 'registration' renamed to 'authorization'

--- MelnykovNotes/settings.py
+++ MelnykovNotes/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'registration.apps.RegistrationConfig'
+    'authorization.apps.AuthorizationConfig'
 ]
 
 MIDDLEWARE = [
