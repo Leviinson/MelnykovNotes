393c7d926a5c0bc4a3391d1c10e4e186ad7150dd
Vitalii <votosi197@gmail.com>
2022-11-13 15:08:40 +0100

    updated: registration view

--- authorization/views.py
+++ authorization/views.py
@@ -5,22 +5,14 @@ from .forms import RegisterUserForm
 
 # Create your views here.
 def register_user(request):
-
-    if request.method == 'POST':
-        dict_form_form = {
-            'username': request.POST['username'],
-            'email': request.POST['email'],
-            'email_conf': request.POST['email_conf'],
-            'password': request.POST['password'],
-            'password_conf': request.POST['password_conf']
-        }
-        reg_form = RegisterUserForm(dict_form_form)
-        if reg_form.is_valid():
-            reg_form.save(commit = True)
-            return redirect('login')
-    else:
-        reg_form = RegisterUserForm()
-        
+    '''
+    Returns empty registration form or
+    registers user in database.
+    '''
+    reg_form = RegisterUserForm(request.POST or None)
+    if reg_form.is_valid():
+        reg_form.save(commit = True)
+        return redirect('login')
     return render(request, 'registration/user_registration_form.html', context = {'reg_form': reg_form, 'title': 'Sign up'})
 
 def login_user(request):
