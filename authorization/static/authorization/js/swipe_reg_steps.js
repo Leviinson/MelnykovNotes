8a902c931d72bd44d47eb9748f66634d8fe2003d
Vitalii <votosi197@gmail.com>
2022-11-18 11:20:40 +0100

    Revert "updated: functions 'swipe_next_reg_step' and 'swipe_prev_reg_step' are now blocked during animation"
    This reverts commit d3b8d6712ab748e30967385f62ef742508a52ad3.

--- authorization/static/authorization/js/swipe_reg_steps.js
+++ authorization/static/authorization/js/swipe_reg_steps.js
@@ -2,21 +2,11 @@ let registration_wrapper = document.getElementById('reg-steps-wrapper');
 let wrapper_width = registration_wrapper.offsetWidth;
 
 function swipe_next_reg_step() {
-    deactivate_swipe_step_button('reg-previous-step__button')
     return registration_wrapper.style.left = registration_wrapper.offsetLeft - wrapper_width + 'px';
 }
 
 function swipe_prev_reg_step() {
-    deactivate_swipe_step_button('reg-next-step__button')
     return registration_wrapper.style.left = registration_wrapper.offsetLeft + wrapper_width  + 'px';
-}
-
-function deactivate_swipe_step_button(button_id) {
-    let swipe_step_btn = document.getElementById(button_id)
-    swipe_step_btn.setAttribute('disabled', '')
-    setTimeout(function () {
-        swipe_step_btn.removeAttribute('disabled')
-    }, 500)
 }
 
 function swipe_to_reg_step_with_errorlist() {
