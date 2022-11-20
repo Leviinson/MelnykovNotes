1a2852369823869de46886dfeb10ef1b29b2f5fa
Nadfkf <m.timofieiew@gmail.com>
2022-11-20 22:08:02 +0100

    added: temporary view for user profile

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,8 +1,20 @@
 from django.http import (
                         HttpRequest,
                         )
+from django.urls import reverse_lazy
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+
+
+import uuid
+
 
 # Create your views here.
-def func_stub_userprofile(request: HttpRequest):
-    return render(request, 'userprofile/profile.html', {})
+@login_required
+def func_stub_userprofile(request: HttpRequest, user_uuid: uuid):
+    if request.user.uuid == user_uuid:
+        pass
+    else:
+        pass
+    return render(request, 'userprofile/profile.html', {'user': get_user_model()})
