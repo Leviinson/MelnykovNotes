1955aa655fac0245fa8d84af8f10cdd12f1384dc
Nadfkf <m.timofieiew@gmail.com>
2022-11-20 22:05:38 +0100

    updated: user can pass to the profile only with specified uuid

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -5,5 +5,5 @@ from django.urls import (
 from .views import func_stub_userprofile
 app_name = 'userprofile'
 urlpatterns = [
-    path('', func_stub_userprofile, name='profile_page')
+    path('<uuid:user_uuid>/', func_stub_userprofile, name='profile_page')
 ]
