e6862646030598c48865c518760e383cae6cee89
Nadfkf <m.timofieiew@gmail.com>
2022-12-03 20:36:41 +0100

    added: table "Allowed friends to view user tasks", fixes

--- userprofile/models.py
+++ userprofile/models.py
@@ -3,13 +3,12 @@ from django.contrib.auth import get_user_model
 from authorization.models import CustomUser
 
 # Create your models here.
-
 class Tasks(models.Model):
     user_id = models.ForeignKey(CustomUser, on_delete = models.CASCADE)
     title = models.CharField(max_length = 30, default = 'My task')
-    description = models.CharField(max_length = 300, blank = True)
+    description = models.CharField(max_length = 300, null = True, blank = True)
     date_created = models.DateTimeField(auto_now_add = True)
-    date_modefied = models.DateTimeField(auto_now = True)
+    date_modified = models.DateTimeField(auto_now = True)
     appointment_date = models.DateTimeField()
 
     class Meta:
@@ -18,7 +17,13 @@ class Tasks(models.Model):
         order_with_respect_to = 'user_id'
         get_latest_by = ['date_created']
 
+
+class AllowedFriendsToTask(models.Model):
+    task_id = models.ForeignKey(Tasks, related_name = '', on_delete = models.CASCADE)
+    friend_id = models.ForeignKey(get_user_model(), on_delete = models.CASCADE)
+
+
 class FriendsRequests(models.Model):
-    from_user = models.ForeignKey(get_user_model(), related_name = 'from_user',on_delete = models.CASCADE)
-    to_user = models.ForeignKey(get_user_model(), related_name = 'to_user', on_delete = models.CASCADE)
+    from_user = models.ForeignKey(get_user_model(), on_delete = models.CASCADE)
+    to_user = models.ForeignKey(get_user_model(), on_delete = models.CASCADE)
     relationship = models.SmallIntegerField(null = False, blank = False)
