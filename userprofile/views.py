ea32696ff0b1439ce53b40e293dfb5d4167fc702
Levinson <votosi197@gmail.com>
2022-12-23 17:07:32 +0100

    added: logic to return tasks by specified period in the URL

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,34 +1,62 @@
-from django.http import (HttpRequest,
+from django.http import (Http404,
+                         HttpRequest,
+                         HttpResponseNotFound,
                          HttpResponse)
 
 from django.urls import reverse_lazy
 from django.shortcuts import render
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from django.conf import settings
+
+from dateutil.relativedelta import relativedelta
+from datetime import date 
+
+import uuid
 
 from .models import (Tasks,
                      FriendsRequests,
                      AllowedFriendsToTask)
-
-
-import uuid
-
+from .services import define_period_for_relativedelta
 
 # Create your views here.
 @login_required
-def func_stub_userprofile(request: HttpRequest, user_uuid: uuid):
+def show_userprofile(request: HttpRequest,
+                     user_uuid: uuid.UUID,
+                     period: str = settings.SORT_TASKS_LD):
     '''
     Returns the user's profile depending on
     whether he is the owner or not returns "owner" variable
     with boolean value.
     '''
+
+    if period not in (
+        settings.SORT_TASKS_LD,
+        settings.SORT_TASKS_LW,
+        settings.SORT_TASKS_LM,
+        settings.SORT_TASKS_LY,
+        settings.SORT_TASKS_AT
+    ): raise Http404("404 Error...")
+
     users = get_user_model()
     user_profile = users.objects.get(uuid = user_uuid)
     
-    if request.user.uuid == user_uuid: is_owner = True
-    else: is_owner = False
+    match request.user.uuid == user_uuid:  
+        case True:
+            is_owner = True
 
+        case False:
+            is_owner = False
+    
+    time_delta = define_period_for_relativedelta(period)
+    user_tasks_by_period = Tasks.objects.filter(user_id = user_profile.pk,
+                                                date_created__gte = timezone.now() - relativedelta(**time_delta))
     return render(request, 'userprofile/profile.html', {'user': user_profile,
-                                                        'tasks': Tasks,
+                                                        'tasks': user_tasks_by_period,
                                                         'friends': user_profile.friends.all(),
                                                         'owner': is_owner})
+
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound("404 error")
