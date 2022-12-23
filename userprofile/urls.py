6c6a128c0b5a469ce75fde4151e35f0c2c12d5a4
Levinson <votosi197@gmail.com>
2022-12-23 17:06:45 +0100

    added: path for case when 'period' argument for 'profile_page' wasn't specified

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -1,7 +1,11 @@
 from django.urls import (path,
                          include)
-from .views import func_stub_userprofile
+from .views import (show_userprofile,
+                    page_not_found) 
 app_name = 'userprofile'
 urlpatterns = [
-    path('<uuid:user_uuid>/', func_stub_userprofile, name='profile_page')
+        path('<uuid:user_uuid>/<slug:period>/', show_userprofile, name = 'profile_page_with_period'),
+        path('<uuid:user_uuid>/', show_userprofile, name = 'profile_page_without_period')
 ]
+
+handler404 = page_not_found
