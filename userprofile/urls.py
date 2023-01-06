96b7f166e8bc85967dc701a052f52570ce27ff53
Levinson <votosi197@gmail.com>
2023-01-06 20:05:46 +0100

    fix

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -4,8 +4,8 @@ from .views import (show_userprofile,
                     page_not_found) 
 app_name = 'userprofile'
 urlpatterns = [
-        path('<uuid:user_uuid>/<slug:period_abbreviature>/', show_userprofile, name = 'profile_page_with_period'),
-        path('<uuid:user_uuid>/', show_userprofile, name = 'profile_page_without_period')
+        path('<uuid:requested_user_uuid>/<slug:period_abbreviature>/', show_userprofile, name = 'profile_page_with_period'),
+        path('<uuid:requested_user_uuid>/', show_userprofile, name = 'profile_page_without_period')
 ]
 
 handler404 = page_not_found
